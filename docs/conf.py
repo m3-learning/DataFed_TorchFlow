35f3063b13bd23a5091cefc7df6088c905ba15df
jagar2 <jca92@drexel.edu>
2024-09-16 14:58:03 +0000

    update config for docs

--- docs/conf.py
+++ docs/conf.py
@@ -153,14 +153,26 @@ todo_emit_warnings = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "alabaster"
+html_theme = "pydata_sphinx_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
+# Material theme options (see theme.conf for more information)
 html_theme_options = {
-    "sidebar_width": "300px",
-    "page_width": "1200px"
+    "show_nav_level": 4,
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "m3-learning/DataFed_TorchFlow",
+            "icon": "fab fa-github-square",
+            "type": "fontawesome",
+        }
+    ],
+    "logo": {
+        "image_light": "_static/Drexel_blue_Logo_square_Light.png",
+        "image_dark": "_static/Drexel_blue_Logo_square_Dark.png",
+    },
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
@@ -187,9 +199,13 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 
+html_css_files = [
+    "custom.css",
+]
+
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
-# html_last_updated_fmt = '%b %d, %Y'
+html_last_updated_fmt = "%b %d, %Y"
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
