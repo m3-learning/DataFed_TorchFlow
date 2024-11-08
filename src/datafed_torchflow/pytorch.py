bbd68511fc94385f15fe702cf4f956384ae12af7
JGoddy <jg3837@drexel.edu>
2024-11-08 18:26:52 +0000

    Fixed two bugs regarding filling out the metadata while being flexible enough to find the optimizer either inside or outside `model_dict' . It works now

--- src/datafed_torchflow/pytorch.py
+++ src/datafed_torchflow/pytorch.py
@@ -193,8 +193,8 @@ class TorchLogger:
                 and "globus" not in key.casefold()
                 and "data".casefold() not in str(type(value)).casefold()
                 and "dataloader".casefold() not in str(type(value)).casefold()
-                and not (callable(value) and key not in model_architecture_names)
-                and not (callable(value) and key not in ["optimizer","optim","optim_","optimizer_"])
+                and (not (callable(value) and key not in model_architecture_names)
+                or not (callable(value) and key not in ["optimizer","optim","optim_","optimizer_"]))
                 and type(value)
                 not in [
                     type,
@@ -208,17 +208,28 @@ class TorchLogger:
             ):
                     
                 # put the model architecture into the Model Architecture sub-dictionary
-                if key in model_architecture_names:
-                    
-                # serialize the model architecture blocks (encoder, decoder, etc. )
-                    DataFed_record_metadata["Model Parameters"][
-                        "Model Architecture"
-                    ][key] = serialize_model(value)
-                    DataFed_record_metadata["Model Parameters"][
-                        "Model Architecture"
-                    ][key].update(extract_instance_attributes(obj=value))
+                if key in model_architecture_names:   
+                    # serialize the optimizer
+                    if not isinstance(value, str) and key.casefold() in [
+                        "optimizer",
+                        "optim",
+                        "optim_",
+                        "optimizer_"
+                    ]:  # accept "optimizer" or "optim" for flexibility
+                        DataFed_record_metadata["Model Parameters"][
+                            "Model Architecture"
+                        ][key] = serialize_pytorch_optimizer(value)
                 
-            # serialize the optimizer
+                    else:
+                    # serialize the model architecture blocks (encoder, decoder, etc. )
+                        DataFed_record_metadata["Model Parameters"][
+                            "Model Architecture"
+                        ][key] = serialize_model(value)
+                        DataFed_record_metadata["Model Parameters"][
+                            "Model Architecture"
+                        ][key].update(extract_instance_attributes(obj=value))
+                
+                # serialize the optimizer if not in model_dict
                 elif not isinstance(value, str) and key.casefold() in [
                     "optimizer",
                     "optim",
