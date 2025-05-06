c09e59abb9f0d96c4a995a88bef7a69293dbcc63
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2025-05-06 13:15:22 -0400

    Fix overload function signature

--- src/datafed_torchflow/JSON.py
+++ src/datafed_torchflow/JSON.py
@@ -8,7 +8,7 @@ class UniversalEncoder(json.JSONEncoder):
     A custom JSON encoder that can handle numpy data types, sets, and objects with __dict__ attributes.
     """
 
-    def default(self, obj):
+    def default(self, o):
         """
         Override the default method to provide custom serialization for unsupported data types.
 
@@ -19,16 +19,16 @@ class UniversalEncoder(json.JSONEncoder):
         any: The serialized form of the object.
         """
         # Convert numpy types to their Python equivalents
-        if isinstance(obj, np.integer):
-            return int(obj)  # Convert numpy integers to Python int
-        elif isinstance(obj, np.floating):
-            return float(obj)  # Convert numpy floats to Python float
-        elif isinstance(obj, np.ndarray):
-            return obj.tolist()  # Convert numpy arrays to lists
-        elif isinstance(obj, set):
-            return list(obj)  # Convert sets to lists
-        elif hasattr(obj, "__dict__"):
-            return obj.__dict__  # Serialize object attributes
+        if isinstance(o, np.integer):
+            return int(o)  # Convert numpy integers to Python int
+        elif isinstance(o, np.floating):
+            return float(o)  # Convert numpy floats to Python float
+        elif isinstance(o, np.ndarray):
+            return o.tolist()  # Convert numpy arrays to lists
+        elif isinstance(o, set):
+            return list(o)  # Convert sets to lists
+        elif hasattr(o, "__dict__"):
+            return o.__dict__  # Serialize object attributes
         else:
             # Call the default method for other cases
-            return super().default(obj)
+            return super().default(o)
